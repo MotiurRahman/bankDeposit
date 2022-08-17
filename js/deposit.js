2aac09134fc35a74a79f9e55eb64cb55f573ade8
RRO (RSL-Md. Motiur Rahman) <RRO@rahimafrooz.com>
2022-08-17 11:20:48 +0600

    update code

--- js/deposit.js
+++ js/deposit.js
@@ -7,6 +7,7 @@ document.getElementById("deposit").addEventListener('click', function () {
     const totalAmount = document.getElementById("totalAmount").innerText;
 
     console.log(depositAmount, depositCurValue);
+    document.getElementById("depositAmount").value = "";
 
     document.getElementById("depositCurValue").innerText = parseInt(depositCurValue) + parseInt(depositAmount);
     document.getElementById("totalAmount").innerText = parseInt(totalAmount) + parseInt(depositAmount);
@@ -18,6 +19,14 @@ document.getElementById("withdraw").addEventListener('click', function () {
     const withdrawAmount = document.getElementById("withdrawAmount").value;
     const totalWithdraw = document.getElementById("totalWithdraw").innerText;
     const totalAmount = document.getElementById("totalAmount").innerText;
+    document.getElementById("withdrawAmount").value = "";
+
+    if (parseInt(withdrawAmount) > parseInt(totalAmount)) {
+        alert("Not enought balance");
+        return false;
+    }
+
+
 
     document.getElementById("totalWithdraw").innerText = parseInt(totalWithdraw) + parseInt(withdrawAmount);
     document.getElementById("totalAmount").innerText = parseInt(totalAmount) - parseInt(withdrawAmount);
